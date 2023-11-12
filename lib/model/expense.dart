1b441a56c0f775a2abf88f3057da6270e6594fa5
Nagaaswin S <nagaaswin775@gmail.com>
2023-11-12 20:52:01 +0530

    added category enum

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -1,12 +1,18 @@
+import 'package:expenses_tracker_app/enum/category.dart';
 import 'package:uuid/uuid.dart';
 
-const uuid = Uuid();
+const Uuid uuid = Uuid();
 
 class Expense {
-  Expense({required this.title, required this.amount, required this.date})
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
       : id = uuid.v4(); // constructor initializer
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
