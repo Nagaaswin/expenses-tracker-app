d0b9bccd49b26170b145252932b2071a91a1f0e3
Nagaaswin S <nagaaswin775@gmail.com>
2023-11-18 21:42:08 +0530

    developed widget based theme data

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,30 @@ void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: kColorScheme,
-          appBarTheme: const AppBarTheme().copyWith(
-            foregroundColor: kColorScheme.primaryContainer,
-            backgroundColor: kColorScheme.onPrimaryContainer,
-          )),
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          foregroundColor: kColorScheme.primaryContainer,
+          backgroundColor: kColorScheme.onPrimaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.secondaryContainer),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: const TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 14,
+              ),
+            ),
+      ),
       home: const Expenses(),
     ),
   );
